20fcab96ca571d00f5332a40c16a2d51c7e4dd32
Julian Horoszkiewicz <bliskometalu@protonmail.com>
2020-05-03 14:49:12 +0200

    Update low_hanging_search.sh

--- low_hanging_search.sh
+++ low_hanging_search.sh
@@ -1,3 +1,3 @@
-# grep the output for lowest-hanging fruits
+# grep the output for lowest-hanging fruits (pure high confidence stuff, no tentative, no terminator-required, no second-order)
 GREPLOG=$1
-grep '\[EXEC\]\|\[XSS\]\|\[SQL\]\|\[EVAL\]\|\[FOPEN\]\|\[SHELL\]' $GREPLOG
+grep '\[EXEC\]\|\[XSS\]\|\[SQL\]\|\[EVAL\]\|\[FOPEN\]\|\[SHELL\]|\[UPLOAD\]' $GREPLOG
